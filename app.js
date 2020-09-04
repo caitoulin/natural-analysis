43d7d51d005c7c7b6711cb3a70f11e193ed06c21
caitoulin <1218182878@qq.com>
2020-09-04 15:44:13 +0800

    feat:增加express代码

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var ejs = require('ejs');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const mainRouter = require('./routes/router');
+const app = express();
+const options = {
+    etag: true,
+    lastModified: false,
+    setHeaders: (res, path, stat) => {
+        res.set({
+            'Cache-Control': 'max-age=00',
+        });
+    },
+};
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(express.static(path.join(__dirname, 'public'), options));
 
-var app = express();
-
-app.engine('html', ejs.renderFile);
-app.set('views', path.join(__dirname, './views/dist')); //html文件加载路径
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, './views/dist')));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,request-origin');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+app.use('/get', mainRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
@@ -34,7 +33,6 @@ app.use(function (err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500);
     res.render('error');
