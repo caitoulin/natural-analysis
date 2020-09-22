0fd0ce46f9280be0d30642b87cb3c12dd267b75b
caitoulin <1218182878@qq.com>
2020-09-22 17:39:22 +0800

    feat:9.22.1

--- views/components/mainMap/MainFlow.tsx
+++ views/components/mainMap/MainFlow.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
 import OSM from 'ol/source/OSM';
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorSource from 'ol/source/Vector';
 import './main.less';
 import Menu from '../menuBar/Menu';
 import BasisBar from '../basisBar/basisBar';
+import * as turf from '@turf/turf';
+import {
+    BBox,
+    Units,
+    Feature,
+    Polygon,
+    MultiPolygon,
+    Properties,
+} from '@turf/turf';
 declare global {
     interface Window {
         LDmap: any;
     }
+}
+
+interface Option {
+    units?: Units;
+    properties?: Properties;
+    mask?: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon;
 }
 
 export default class MainFlow extends React.Component<any, any> {
@@ -17,8 +35,9 @@ export default class MainFlow extends React.Component<any, any> {
         window.LDmap = new Map({
             controls: [],
             view: new View({
-                center: [0, 0],
-                zoom: 1,
+                center: [123.474903, 24.223405],
+                zoom: 4,
+                projection: 'EPSG:4326',
             }),
             layers: [
                 new TileLayer({
@@ -27,7 +46,25 @@ export default class MainFlow extends React.Component<any, any> {
             ],
             target: 'map',
         });
+        this.defaultGrids();
     }
+    defaultGrids = (): void => {
+        const bbox: BBox = [98, 17.5, 124, 46];
+        const cellSide = 150;
+        const options: Option = { units: 'miles' };
+        const squareGrid = turf.squareGrid(bbox, cellSide, options);
+        const format = new GeoJSON();
+        const source = new VectorSource();
+        const allFeatures = format
+            .readFeatures(squareGrid)
+            .map((feature, index) => {
+                feature.setId(index);
+                return feature;
+            });
+        source.addFeatures(allFeatures);
+        const getLayer = new VectorLayer({ source: source });
+        window.LDmap.addLayer(getLayer);
+    };
     render() {
         return (
             <div className="container">
